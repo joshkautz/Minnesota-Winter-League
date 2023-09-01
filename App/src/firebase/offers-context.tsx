de32411670313d11c0a72807b78eaeab33fd730b
Josh Kautz <josh@joshkautz.com>
2023-09-01 13:34:55 -0500

    Formatting

--- App/src/firebase/offers-context.tsx
+++ App/src/firebase/offers-context.tsx
@@ -20,7 +20,7 @@ interface AuthProps {
 		| undefined
 	outgoingOffersCollectionDataLoading: boolean
 	outgoingOffersCollectionDataError: FirestoreError | undefined
-  incomingOffersCollectionDataSnapshot:
+	incomingOffersCollectionDataSnapshot:
 		| QuerySnapshot<DocumentData, DocumentData>
 		| undefined
 	incomingOffersCollectionDataLoading: boolean
@@ -47,15 +47,14 @@ const OffersContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<OffersContext.Provider
 			value={{
-
-        outgoingOffersCollectionDataSnapshot:
+				outgoingOffersCollectionDataSnapshot:
 					outgoingOffersCollectionDataSnapshot,
 				outgoingOffersCollectionDataLoading:
 					outgoingOffersCollectionDataLoading,
 				outgoingOffersCollectionDataError: outgoingOffersCollectionDataError,
 				incomingOffersCollectionDataSnapshot:
 					incomingOffersCollectionDataSnapshot,
-        incomingOffersCollectionDataLoading:
+				incomingOffersCollectionDataLoading:
 					incomingOffersCollectionDataLoading,
 				incomingOffersCollectionDataError: incomingOffersCollectionDataError,
 			}}
