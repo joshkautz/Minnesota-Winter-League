4ca5f8066a40eaeb69397200ddefae67dd1e9dd8
Josh Kautz <josh@joshkautz.com>
2023-09-13 18:26:30 -0500

    I chat GPT-ed some shit to start populating this page. Gonna need to edit this.

--- App/src/components/home.tsx
+++ App/src/components/home.tsx
@@ -8,6 +8,113 @@ export const Home = () => {
 			>
 				Home
 			</div>
+			<header>
+				<h1>Welcome to Winter League!</h1>
+			</header>
+
+			<main>
+				<section id="league-details">
+					<h2>League Details</h2>
+					<ul>
+						<li>
+							<strong>When:</strong> Saturdays from 6:00 PM to 9:00 PM during
+							November and December
+						</li>
+						<li>
+							<strong>What:</strong> 5v5 Indoor Open Ultimate League
+						</li>
+						<li>
+							<strong>Where:</strong> University of Minnesota | URW Sports Field
+							Complex
+						</li>
+						<li>
+							<strong>Games:</strong> Two 45-minute games every night
+						</li>
+					</ul>
+				</section>
+				<section id="why-join">
+					<h2>Why Join Us?</h2>
+					<p>
+						<strong>Friends:</strong> Form your own team with friends or join as
+						an individual, and experience the camaraderie of sportsmanship
+						against a variety of competitors.
+					</p>
+					<p>
+						<strong>Affordable:</strong> Just $60 per person for 6 nights of
+						play.
+					</p>
+					<p>
+						<strong>Competition:</strong> The league is open to all skill
+						levels, and is focused on making sure everone has valuable
+						off-season opportunities to continue playing, improving, and
+						competing. Weekly play will be organized, and results will be used
+						to ensure that teams are matched up against opponents of similar
+						skill levels. At the end of the season, the top teams will compete
+						in a playoff tournament to determine the league champion!
+					</p>
+        </section>
+        <br></br>
+				<section id="individual-players">
+					<h2>Individual Players</h2>
+					<p>
+						If you're a solo player looking for a team, don't worry! We'll help
+						you find like-minded individuals to team up with and create a squad
+						that's ready to dominate the frisbee field.
+					</p>
+				</section>
+				<section id="team-captains">
+					<h2>Team Captains</h2>
+					<p>
+						In its inaugural year, the league has room for 12 teams. Act fast
+						and register your team before space fills up. There is a 10-player
+						minimum for teams, with no roster limit. Team registration requires
+						a flat $100 fee for participation. Team captains, assemble your
+						dream team and sign up together to ensure your place in the league.
+						Show off your strategic skills and lead your team to victory!
+					</p>
+				</section>
+				<section id="what-to-expect">
+					<h2>What to Expect</h2>
+					<p>
+						Get ready for action-packed evenings filled with thrilling frisbee
+						matches. Our league is all about sportsmanship, friendly
+						competition, and a whole lot of fun. Whether you're perfecting your
+						throws, working on your defense, or just out to have a great time,
+						the Saturday Frisbee League is the place to be.
+					</p>
+				</section>
+				<section id="how-to-register">
+					<h2>How to Register</h2>
+					<ol>
+						<li>Click on the "Register" button.</li>
+						<li>
+							Choose whether you're registering as an individual or a team.
+						</li>
+						<li>Complete the registration form with your details.</li>
+						<li>Make your payment securely online.</li>
+						<li>Await confirmation and league updates via email.</li>
+					</ol>
+				</section>
+			</main>
+			<footer>
+				<p>
+					Join us this season for unforgettable Saturday nights of frisbee
+					action. Whether you're in it for the love of the game, the chance to
+					win, or simply to enjoy the company of fellow frisbee enthusiasts, we
+					can't wait to welcome you to the league.
+				</p>
+				<p>
+					Don't miss out on this opportunity to be a part of something special.
+					Secure your spot today, and let's make this season of the Saturday
+					Frisbee League one to remember!
+				</p>
+				<p>
+					Got questions or need assistance? Contact us at{' '}
+					<a href="mailto:contact@example.com">contact@example.com</a> or call{' '}
+					<a href="tel:+123456789">123-456-789</a>.
+				</p>
+				<p>See you on the field!</p>
+			</footer>
 		</div>
 	)
 }
