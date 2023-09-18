b0f39dc4e042926d1fa705e6dc83e52495dab663
Josh Kautz <josh@joshkautz.com>
2023-09-18 13:13:26 -0500

    Handled some weird title flickering while state changes. Just a simple, lazy fix.

--- App/src/components/profile.tsx
+++ App/src/components/profile.tsx
@@ -99,9 +99,7 @@ export const Profile = () => {
 					'max-w-max my-4 text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary to-sky-300'
 				}
 			>
-				{documentSnapshot?.data()?.firstname
-					? `Hello ${documentSnapshot?.data()?.firstname}`
-					: 'Your Profile'}
+				Edit Profile
 			</div>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
