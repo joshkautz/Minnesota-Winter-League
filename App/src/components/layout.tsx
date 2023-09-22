ce08c6673b33ce6bde730c3655101faeb7e018ed
nrsimonelli <nrsimonelli@gmail.com>
2023-09-22 11:01:14 -0500

    added global "footer" padding for all routes besides home

--- App/src/components/layout.tsx
+++ App/src/components/layout.tsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { TopNav } from '@/components/top-nav'
+import { cn } from '@/lib/utils'
 
 export const Layout = () => {
+	const { pathname } = useLocation()
+
 	return (
-		<div className={'flex flex-col items-center justify-start min-h-screen'}>
+		<div
+			className={cn(
+				'flex flex-col items-center justify-start min-h-screen',
+				pathname !== '/' && 'pb-10'
+			)}
+		>
 			<TopNav title={'🥏'} />
 			<Outlet />
 		</div>
