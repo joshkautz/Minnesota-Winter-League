cc5a9d6c0b0e7eec397555dfd5ee9ef282242f11
nrsimonelli <nrsimonelli@gmail.com>
2023-10-02 20:34:48 -0500

    placeholder card for empty states

---
+++ App/src/components/coming-soon.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import { Card, CardContent, CardHeader } from './ui/card'
+import { cn } from '@/lib/utils'
+
+export const ComingSoon = ({
+	message,
+	className,
+}: {
+	message: ReactNode
+	className?: string
+}) => {
+	return (
+		<Card className={cn('max-w-[800px] w-full mx-auto', className)}>
+			<CardHeader className="items-center justify-center h-40 text-2xl font-bold rounded-t-lg text-background md:h-60 bg-gradient-to-r from-primary to-sky-300">
+				Coming Soon
+			</CardHeader>
+			<CardContent>
+				<p className={' pt-6 '}>{message}</p>
+			</CardContent>
+		</Card>
+	)
+}
