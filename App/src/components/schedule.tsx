77f5f75d6a30bb58537c1329572dae08979696de
Josh Kautz <josh@joshkautz.com>
2023-12-10 23:26:41 -0600

    Updating hardcoded game results

--- App/src/components/schedule.tsx
+++ App/src/components/schedule.tsx
@@ -64,24 +64,24 @@ const placeholderPlayoffData = [
 	// 	],
 	// },
 
-	{
-		title: 'Week 6 | Round 1 | Semifinals',
-		date: 'Saturday, December 16 at 6:05 PM',
-		content: [
-			{ field: 'Field 1', home: 'P1, 1st', away: 'P4, 1st' },
-			{ field: 'Field 2', home: 'P1, 2nd', away: 'P4, 2nd' },
-			{ field: 'Field 3', home: 'P1, 3rd', away: 'P4, 3rd' },
-		],
-	},
-	{
-		title: 'Week 6 | Round 2 | Semifinals',
-		date: 'Saturday, December 16 at 6:50 PM',
-		content: [
-			{ field: 'Field 1', home: 'P2, 1st', away: 'P3, 1st' },
-			{ field: 'Field 2', home: 'P2, 2nd', away: 'P3, 2nd' },
-			{ field: 'Field 3', home: 'P2, 3rd', away: 'P3, 3rd' },
-		],
-	},
+	// {
+	// 	title: 'Week 6 | Round 1 | Semifinals',
+	// 	date: 'Saturday, December 16 at 6:05 PM',
+	// 	content: [
+	// 		{ field: 'Field 1', home: 'P1, 1st', away: 'P4, 1st' },
+	// 		{ field: 'Field 2', home: 'P1, 2nd', away: 'P4, 2nd' },
+	// 		{ field: 'Field 3', home: 'P1, 3rd', away: 'P4, 3rd' },
+	// 	],
+	// },
+	// {
+	// 	title: 'Week 6 | Round 2 | Semifinals',
+	// 	date: 'Saturday, December 16 at 6:50 PM',
+	// 	content: [
+	// 		{ field: 'Field 1', home: 'P2, 1st', away: 'P3, 1st' },
+	// 		{ field: 'Field 2', home: 'P2, 2nd', away: 'P3, 2nd' },
+	// 		{ field: 'Field 3', home: 'P2, 3rd', away: 'P3, 3rd' },
+	// 	],
+	// },
 	{
 		title: 'Week 6 | Round 3 | 3rd, 7th, 11th Finals',
 		date: 'Saturday, December 16 at 7:35 PM',
