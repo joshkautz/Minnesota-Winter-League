725495af223dfb5661e0213bd0271f084ddb507e
Josh Kautz <josh@joshkautz.com>
2024-08-02 22:23:58 -0500

    I'm just fuckin it all up baby I can't be stopped

---
+++ App/eslint.config.js
@@ -0,0 +1,9 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	react.configs.recommended
+)
