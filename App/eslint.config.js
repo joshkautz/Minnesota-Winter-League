de7159f24ec8f6979b45eded7409ba37f9dd9a01
Josh Kautz <josh@joshkautz.com>
2024-08-02 22:48:45 -0500

    Updated flat file eslint config

--- App/eslint.config.js
+++ App/eslint.config.js
@@ -5,5 +5,24 @@ import react from 'eslint-plugin-react'
 export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
-	react.configs.recommended
+	react.configs.flat.recommended,
+	{
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			'no-undef': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'react/jsx-no-target-blank': 'off',
+			'react/prop-types': 'off',
+			'react/no-unescaped-entities': 'off',
+			'react/jsx-uses-react': 'off',
+			'@typescript-eslint/no-unused-expressions': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+		},
+	}
 )
