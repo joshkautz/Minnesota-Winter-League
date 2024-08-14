d7f3887a937f75ba292f53a8398812c77bb346bd
nrsimonelli <nrsimonelli@gmail.com>
2024-08-14 14:44:55 -0500

    default value and skeleton

--- App/src/components/season-select.tsx
+++ App/src/components/season-select.tsx
@@ -6,12 +6,22 @@ import {
 	SelectValue,
 } from '@/components/ui/select'
 import { useSeasonContext } from '@/firebase/season-context'
+import { useEffect, useState } from 'react'
+import { Skeleton } from './ui/skeleton'
 
 export const SeasonSelect = () => {
-	const { selectedSeason, setSelectedSeason, seasonsQuerySnapshot } =
-		useSeasonContext()
+	const {
+		selectedSeason,
+		setSelectedSeason,
+		seasonsQuerySnapshot,
+		seasonsQuerySnapshotLoading,
+	} = useSeasonContext()
+
+	const [stringValue, setStringValue] = useState<string | undefined>(undefined)
+	const [isLoaded, setIsLoaded] = useState(false)
 
 	const handleSeasonChange = (season: string) => {
+		setStringValue(season)
 		const seasonDoc = seasonsQuerySnapshot?.docs.find(
 			(doc) => doc.data().name === season
 		)
@@ -20,25 +30,32 @@ export const SeasonSelect = () => {
 		}
 	}
 
+	useEffect(() => {
+		if (!seasonsQuerySnapshotLoading && selectedSeason && !isLoaded) {
+			setStringValue(selectedSeason.data().name)
+			setIsLoaded(true)
+		}
+	}, [seasonsQuerySnapshotLoading, selectedSeason, isLoaded])
+
 	return (
 		<div className="inline-flex items-center justify-center py-16 space-x-2">
 			<p>Season</p>
-
-			<Select onValueChange={handleSeasonChange}>
-				<SelectTrigger>
-					<SelectValue
-						defaultValue={selectedSeason?.data().name}
-						placeholder={'Select a season'}
-					/>
-				</SelectTrigger>
-				<SelectContent>
-					{seasonsQuerySnapshot?.docs.map((season) => (
-						<SelectItem key={season.id} value={season.data().name}>
-							{season.data().name}
-						</SelectItem>
-					))}
-				</SelectContent>
-			</Select>
+			{seasonsQuerySnapshotLoading ? (
+				<Skeleton className="w-24 h-8" />
+			) : (
+				<Select value={stringValue} onValueChange={handleSeasonChange}>
+					<SelectTrigger>
+						<SelectValue />
+					</SelectTrigger>
+					<SelectContent>
+						{seasonsQuerySnapshot?.docs.map((season) => (
+							<SelectItem key={season.id} value={season.data().name}>
+								{season.data().name}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			)}
 		</div>
 	)
 }
