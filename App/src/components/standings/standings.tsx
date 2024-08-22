9f6d17314492ae173a8b456ef82c45716dc64177
nrsimonelli <nrsimonelli@gmail.com>
2024-08-22 14:31:59 -0500

    standings refactor

--- App/src/components/standings/standings.tsx
+++ App/src/components/standings/standings.tsx
@@ -7,91 +7,20 @@ import {
 	TableHead,
 	TableBody,
 	TableCell,
-} from './ui/table'
-import { useMemo } from 'react'
-import { GradientHeader } from './gradient-header'
-import { ComingSoon } from './coming-soon'
+} from '../ui/table'
+import { GradientHeader } from '../gradient-header'
+import { ComingSoon } from '../coming-soon'
 import { cn } from '@/lib/utils'
 import { useTeamsContext } from '@/firebase/teams-context'
 import { useGamesContext } from '@/firebase/games-context'
 import { ReloadIcon } from '@radix-ui/react-icons'
+import { useStandings } from '@/lib/use-standings'
 
 export const Standings = () => {
 	const { teamsQuerySnapshot } = useTeamsContext()
 	const { gamesQuerySnapshot } = useGamesContext()
 
-	const standings = useMemo(() => {
-		const result: {
-			[key: string]: {
-				pointsFor: number
-				pointsAgainst: number
-				wins: number
-				losses: number
-				differential: number
-			}
-		} = {}
-
-		gamesQuerySnapshot?.docs.forEach((gameQueryDocumentSnapshot) => {
-			const home = gameQueryDocumentSnapshot.data().home
-			const away = gameQueryDocumentSnapshot.data().away
-
-			result[home.id] = {
-				pointsFor:
-					(result[home.id] ? result[home.id].pointsFor : 0) +
-					gameQueryDocumentSnapshot.data().homeScore,
-				pointsAgainst:
-					(result[home.id] ? result[home.id].pointsAgainst : 0) +
-					gameQueryDocumentSnapshot.data().awayScore,
-				differential:
-					(result[home.id] ? result[home.id].differential : 0) +
-					gameQueryDocumentSnapshot.data().homeScore -
-					gameQueryDocumentSnapshot.data().awayScore,
-				wins:
-					gameQueryDocumentSnapshot.data().homeScore >
-					gameQueryDocumentSnapshot.data().awayScore
-						? (result[home.id] ? result[home.id].wins : 0) + 1
-						: result[home.id]
-							? result[home.id].wins
-							: 0,
-				losses:
-					gameQueryDocumentSnapshot.data().homeScore >
-					gameQueryDocumentSnapshot.data().awayScore
-						? result[home.id]
-							? result[home.id].losses
-							: 0
-						: (result[home.id] ? result[home.id].losses : 0) + 1,
-			}
-
-			result[away.id] = {
-				pointsFor:
-					(result[away.id] ? result[away.id].pointsFor : 0) +
-					gameQueryDocumentSnapshot.data().awayScore,
-				pointsAgainst:
-					(result[away.id] ? result[away.id].pointsAgainst : 0) +
-					gameQueryDocumentSnapshot.data().homeScore,
-				differential:
-					(result[away.id] ? result[away.id].differential : 0) +
-					gameQueryDocumentSnapshot.data().awayScore -
-					gameQueryDocumentSnapshot.data().homeScore,
-				wins:
-					gameQueryDocumentSnapshot.data().awayScore >
-					gameQueryDocumentSnapshot.data().homeScore
-						? (result[away.id] ? result[away.id].wins : 0) + 1
-						: result[away.id]
-							? result[away.id].wins
-							: 0,
-				losses:
-					gameQueryDocumentSnapshot.data().awayScore >
-					gameQueryDocumentSnapshot.data().homeScore
-						? result[away.id]
-							? result[away.id].losses
-							: 0
-						: (result[away.id] ? result[away.id].losses : 0) + 1,
-			}
-		})
-
-		return result
-	}, [gamesQuerySnapshot, teamsQuerySnapshot])
+	const standings = useStandings(gamesQuerySnapshot)
 
 	const getColor = (value: number) => {
 		if (value > 9) {
