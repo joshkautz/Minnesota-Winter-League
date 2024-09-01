c451e9ee6edac825309a326d3b849420b61f5289
nrsimonelli <nrsimonelli@gmail.com>
2024-08-31 19:41:43 -0500

    loading spinner and no more skeleton

--- App/src/components/unrostered-player-list.tsx
+++ App/src/components/unrostered-player-list.tsx
@@ -7,10 +7,8 @@ import {
 } from '@/firebase/firestore'
 import { useMemo, useState } from 'react'
 import { NotificationCard } from './notification-card'
-import { Button } from './ui/button'
 import { toast } from './ui/use-toast'
 import { PlayerData, TeamData } from '@/lib/interfaces'
-import { Skeleton } from './ui/skeleton'
 import { UnrosteredPlayerDetail } from './unrostered-player-detail'
 import { UnrosteredPlayerSearchBar } from './unrostered-player-search-bar'
 import { usePlayersSearch } from './use-players-search'
@@ -60,27 +58,16 @@ export const UnrosteredPlayerList = ({
 			description={'players eligible for team roster invitations.'}
 			scrollArea
 			searchBar={
-				<UnrosteredPlayerSearchBar value={search} onChange={setSearch} />
+				<UnrosteredPlayerSearchBar
+					value={search}
+					onChange={setSearch}
+					searching={playersQuerySnapshotLoading}
+				/>
 			}
 		>
-			{playersQuerySnapshotLoading ? (
-				Array.from({ length: 10 }).map((_, index) => (
-					<Skeleton key={index} className="mb-2">
-						<div className="flex items-end gap-2 py-2">
-							<div className="ml-2">
-								<Skeleton className="w-20 h-5" />
-
-								<Skeleton className="h-5 mt-1 w-36" />
-							</div>
-							<Skeleton className="flex justify-end w-20 gap-2 ml-auto mr-2">
-								<Button size={'sm'} variant={'ghost'} />
-							</Skeleton>
-						</div>
-					</Skeleton>
-				))
-			) : !playersQuerySnapshot ||
-			  playersQuerySnapshot.empty ||
-			  search.length === 0 ? (
+			{!playersQuerySnapshot ||
+			playersQuerySnapshot.empty ||
+			search.length === 0 ? (
 				<span>No players found</span>
 			) : (
 				playersQuerySnapshot?.docs.map((playerQueryDocumentSnapshot, index) => (
--- App/src/components/unrostered-player-search-bar.tsx
+++ App/src/components/unrostered-player-search-bar.tsx
@@ -1,19 +1,29 @@
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { Input } from './ui/input'
 
 export const UnrosteredPlayerSearchBar = ({
 	value,
 	onChange,
+	searching,
 }: {
 	value: string
 	onChange: React.Dispatch<React.SetStateAction<string>>
+	searching: boolean
 }) => {
 	return (
-		<div className={'pt-2'}>
+		<div className={'mt-2 relative'}>
 			<Input
 				placeholder={'Start typing to search...'}
 				value={value}
 				onChange={(e) => onChange(e.target.value)}
 			/>
+			{searching && (
+				<div className={'absolute inset-y-0 right-3 h-full items-center flex'}>
+					<ReloadIcon
+						className={'w-4 h-4 text-muted-foreground animate-spin'}
+					/>
+				</div>
+			)}
 		</div>
 	)
 }
