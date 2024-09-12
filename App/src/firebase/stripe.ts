cfd9e15d67df5bda9f3403a8a4052843408d53d6
Josh Kautz <josh@joshkautz.com>
2024-09-12 13:32:43 -0500

    Turn off test mode. Use read Stripe product.

--- App/src/firebase/stripe.ts
+++ App/src/firebase/stripe.ts
@@ -1,4 +1,4 @@
 export enum Products {
 	WinterLeagueRegistration2023 = 'price_1NkUnZHMbQIHBzSiRjN2Ms70',
-	WinterLeagueRegistration2024 = 'price_1Pv155HMbQIHBzSiBsjvzb2L', // REAL: price_1PjNZjHMbQIHBzSiEbtC32Rb
+	WinterLeagueRegistration2024 = 'price_1PjNZjHMbQIHBzSiEbtC32Rb',
 }
