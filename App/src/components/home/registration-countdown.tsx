2994e78e92b486778bb6c3f4c2f7a3e1786904e0
Josh Kautz <josh@joshkautz.com>
2024-09-11 21:30:31 -0500

    Fixed my oopsies!

--- App/src/components/home/registration-countdown.tsx
+++ App/src/components/home/registration-countdown.tsx
@@ -6,71 +6,76 @@ const MINUTES = 1000 * 60
 
 export const RegistrationCountdown = () => {
 	const { currentSeasonQueryDocumentSnapshot } = useSeasonsContext()
-
-	const todaysDate = new Date()
-	const futureDate = currentSeasonQueryDocumentSnapshot
-		? currentSeasonQueryDocumentSnapshot?.data().registrationStart.toDate()
-		: new Date()
-
-	const timerEndDate = futureDate.getTime() - todaysDate.getTime()
-	const isRegistrationOpen = timerEndDate <= 0
-	const [remainingTime, setRemainingTime] = useState(timerEndDate)
+	const [remaining, setRemaining] = useState<number>()
 
 	useEffect(() => {
-		const timerInterval = setInterval(() => {
-			setRemainingTime((prevTime) => {
-				if (prevTime === 0) {
-					clearInterval(timerInterval)
-					return 0
-				} else {
-					return prevTime - 1000
-				}
-			})
-		}, 1000)
+		if (currentSeasonQueryDocumentSnapshot === undefined) return
 
-		return () => clearInterval(timerInterval)
-	}, [])
+		const currentDate = new Date()
+		const startDate = currentSeasonQueryDocumentSnapshot
+			?.data()
+			.registrationStart.toDate()
 
-	const days = Math.floor(remainingTime / (HOURS * 24))
-	const hours = Math.floor((remainingTime % (HOURS * 24)) / HOURS)
-	const minutes = Math.floor((remainingTime % HOURS) / MINUTES)
-	const seconds = Math.floor((remainingTime % MINUTES) / 1000)
+		setRemaining(startDate.getTime() - currentDate.getTime())
+
+		const interval = setInterval(
+			() =>
+				setRemaining((prevTime) => {
+					if (prevTime === 0) {
+						clearInterval(interval)
+						return 0
+					} else if (prevTime === undefined) {
+						return startDate.getTime() - currentDate.getTime()
+					} else {
+						return prevTime - 1000
+					}
+				}),
+			1000
+		)
+
+		return () => clearInterval(interval)
+	}, [currentSeasonQueryDocumentSnapshot])
+
+	const days = remaining ? Math.floor(remaining / (HOURS * 24)) : '?'
+	const hours = remaining ? Math.floor((remaining % (HOURS * 24)) / HOURS) : '?'
+	const minutes = remaining ? Math.floor((remaining % HOURS) / MINUTES) : '?'
+	const seconds = remaining ? Math.floor((remaining % MINUTES) / 1000) : '?'
 
 	return (
-		<>
-			{isRegistrationOpen ? (
-				<div className="text-3xl font-bold">Registration open now!</div>
+		<div className="flex flex-col items-start">
+			{remaining === undefined ? (
+				<div className="text-3xl font-bold">Registration...</div>
+			) : remaining <= 0 ? (
+				<div className="text-3xl font-bold">Registration is open!</div>
 			) : (
-				<div className="flex flex-col items-start">
-					<div className="text-3xl font-bold">Registration opens soon!</div>
-					<div className="flex mt-2 space-x-2">
-						<div className="flex flex-col items-center min-w-16">
-							<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
-								{days}
-							</p>
-							<p className="text-sm font-bold">days</p>
-						</div>
-						<div className="flex flex-col items-center min-w-16">
-							<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
-								{hours}
-							</p>
-							<p className="text-sm font-bold">hours</p>
-						</div>
-						<div className="flex flex-col items-center min-w-16">
-							<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
-								{minutes}
-							</p>
-							<p className="text-sm font-bold">minutes</p>
-						</div>
-						<div className="flex flex-col items-center min-w-16">
-							<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
-								{seconds}
-							</p>
-							<p className="text-sm font-bold">seconds</p>
-						</div>
-					</div>
-				</div>
+				<div className="text-3xl font-bold">Registration opens soon!</div>
 			)}
-		</>
+			<div className="flex mt-2 space-x-2">
+				<div className="flex flex-col items-center min-w-16">
+					<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
+						{days}
+					</p>
+					<p className="text-sm font-bold">days</p>
+				</div>
+				<div className="flex flex-col items-center min-w-16">
+					<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
+						{hours}
+					</p>
+					<p className="text-sm font-bold">hours</p>
+				</div>
+				<div className="flex flex-col items-center min-w-16">
+					<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
+						{minutes}
+					</p>
+					<p className="text-sm font-bold">minutes</p>
+				</div>
+				<div className="flex flex-col items-center min-w-16">
+					<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
+						{seconds}
+					</p>
+					<p className="text-sm font-bold">seconds</p>
+				</div>
+			</div>
+		</div>
 	)
 }
