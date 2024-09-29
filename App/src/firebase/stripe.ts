b86807491e65bd43b640dc3d0959a0314c9593ef
Josh Kautz <josh@joshkautz.com>
2024-09-28 19:56:10 -0500

    Using real production product from stripe

--- App/src/firebase/stripe.ts
+++ App/src/firebase/stripe.ts
@@ -1,4 +1,4 @@
 export enum Products {
 	WinterLeagueRegistration2023 = 'price_1NkUnZHMbQIHBzSiRjN2Ms70',
-	WinterLeagueRegistration2024 = 'price_1Pv155HMbQIHBzSiBsjvzb2L',
+	WinterLeagueRegistration2024 = 'price_1PjNZjHMbQIHBzSiEbtC32Rb',
 }
