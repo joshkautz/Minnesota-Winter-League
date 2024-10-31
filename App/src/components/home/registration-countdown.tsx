f432e4ae72f97c8e8a760f54ccb588c534eb3ecc
Josh Kautz <josh@joshkautz.com>
2024-10-31 17:44:46 -0500

    Closing down the home page. Should've done this a while ago. Oops.

--- App/src/components/home/registration-countdown.tsx
+++ App/src/components/home/registration-countdown.tsx
@@ -1,40 +1,40 @@
-import { useSeasonsContext } from '@/contexts/seasons-context'
-import { useEffect, useState } from 'react'
+// import { useSeasonsContext } from '@/contexts/seasons-context'
+// import { useEffect, useState } from 'react'
 
 // const HOURS = 1000 * 60 * 60
 // const MINUTES = 1000 * 60
 
 export const RegistrationCountdown = () => {
-	const { currentSeasonQueryDocumentSnapshot } = useSeasonsContext()
-	const [remaining, setRemaining] = useState<number>()
+	// const { currentSeasonQueryDocumentSnapshot } = useSeasonsContext()
+	// const [remaining, setRemaining] = useState<number>()
 
-	useEffect(() => {
-		if (currentSeasonQueryDocumentSnapshot === undefined) return
+	// useEffect(() => {
+	// 	if (currentSeasonQueryDocumentSnapshot === undefined) return
 
-		const currentDate = new Date()
-		const startDate = currentSeasonQueryDocumentSnapshot
-			?.data()
-			.registrationStart.toDate()
+	// 	const currentDate = new Date()
+	// 	const startDate = currentSeasonQueryDocumentSnapshot
+	// 		?.data()
+	// 		.registrationStart.toDate()
 
-		setRemaining(startDate.getTime() - currentDate.getTime())
+	// 	setRemaining(startDate.getTime() - currentDate.getTime())
 
-		const interval = setInterval(
-			() =>
-				setRemaining((prevTime) => {
-					if (prevTime === 0) {
-						clearInterval(interval)
-						return 0
-					} else if (prevTime === undefined) {
-						return startDate.getTime() - currentDate.getTime()
-					} else {
-						return prevTime - 1000
-					}
-				}),
-			1000
-		)
+	// 	const interval = setInterval(
+	// 		() =>
+	// 			setRemaining((prevTime) => {
+	// 				if (prevTime === 0) {
+	// 					clearInterval(interval)
+	// 					return 0
+	// 				} else if (prevTime === undefined) {
+	// 					return startDate.getTime() - currentDate.getTime()
+	// 				} else {
+	// 					return prevTime - 1000
+	// 				}
+	// 			}),
+	// 		1000
+	// 	)
 
-		return () => clearInterval(interval)
-	}, [currentSeasonQueryDocumentSnapshot])
+	// 	return () => clearInterval(interval)
+	// }, [currentSeasonQueryDocumentSnapshot])
 
 	// const days = remaining ? Math.floor(remaining / (HOURS * 24)) : '?'
 	// const hours = remaining ? Math.floor((remaining % (HOURS * 24)) / HOURS) : '?'
@@ -43,13 +43,14 @@ export const RegistrationCountdown = () => {
 
 	return (
 		<div className="flex flex-col items-start">
-			{remaining === undefined ? (
+			<div className="text-3xl font-bold">Registration is closed!</div>
+			{/* {remaining === undefined ? (
 				<div className="text-3xl font-bold">Registration...</div>
 			) : remaining <= 0 ? (
 				<div className="text-3xl font-bold">Registration is open!</div>
 			) : (
 				<div className="text-3xl font-bold">Registration opens soon!</div>
-			)}
+			)} */}
 			{/* <div className="flex mt-2 space-x-2">
 				<div className="flex flex-col items-center min-w-16">
 					<p className="w-full p-2 text-3xl text-center rounded-lg bg-accent dark:bg-primary text-accent-foreground dark:text-primary-foreground">
